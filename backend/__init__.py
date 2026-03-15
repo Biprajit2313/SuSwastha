0404933fcc1af148344ecc053ccf98e2be93156a
Biprajit Roy <biprajitroy481@gmail.com>
2026-03-15 21:55:12 +0530

    Add files via upload

---
+++ backend/__init__.py
@@ -0,0 +1,8 @@
+"""
+SuSwastha backend package.
+
+This file makes the backend directory a Python package so that modules
+like app, db_models, utils_save, and report_utils can be imported
+reliably in local development and deployment.
+"""
+
